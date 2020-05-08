9635307e862e71d2b2a315d1fdb1dd32a1364311
lewis-weinberger <lweinberger@btinternet.com>
2020-05-08 16:32:59 +0100

    x mod keys

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,7 +1,7 @@
 #ifndef _KEYBOARD_H_
 #define _KEYBOARD_H_ 1
 #if defined(__cplusplus)
-extern "C" { 
+extern "C" {
 #endif
 typedef struct 	Keyboardctl Keyboardctl;
 
@@ -16,31 +16,35 @@ extern	int			ctlkeyboard(Keyboardctl*, char*);
 extern	void			closekeyboard(Keyboardctl*);
 
 enum {
-	KF=	0xF000,	/* Rune: beginning of private Unicode space */
-	/* KF|1, KF|2, ..., KF|0xC is F1, F2, ..., F12 */
-	Khome=	KF|0x0D,
-	Kup=	KF|0x0E,
-	Kpgup=	KF|0x0F,
-	Kprint=	KF|0x10,
-	Kleft=	KF|0x11,
-	Kright=	KF|0x12,
-	Kdown=	0x80,
-	Kview=	0x80,
-	Kpgdown=	KF|0x13,
-	Kins=	KF|0x14,
-	Kend=	KF|0x18,
+	KF=         0xF000, /* Rune: beginning of private Unicode space */
+		                /* KF|1, KF|2, ..., KF|0xC is F1, F2, ..., F12 */
+	Khome=      KF|0x0D,
+	Kup=        KF|0x0E,
+	Kpgup=      KF|0x0F,
+	Kprint=     KF|0x10,
+	Kleft=      KF|0x11,
+	Kright=     KF|0x12,
+	Kdown=      0x80,
+	Kview=      0x80,
+	Kpgdown=    KF|0x13,
+	Kins=       KF|0x14,
+	Kend=       KF|0x18,
 
-	Kalt=		KF|0x15,
-	Kshift=	KF|0x16,
-	Kctl=		KF|0x17,
+	Kalt=       KF|0x15,
+	Kshift=     KF|0x16,
+	Kctl=       KF|0x17,
 
-	Kbs=	0x08,
-	Kdel=	0x7f,
-	Kesc=	0x1b,
-	Keof=	0x04,
+	Kbs=        0x08,
+	Kdel=       0x7f,
+	Kesc=       0x1b,
+	Keof=       0x04,
 
-	Kcmd=	0xF100,	/* Rune: beginning of Cmd+'a', Cmd+'A', etc on Mac */
-	Kshiftctl=	0xF200 /* Rune: beginning of Shift+Ctrl+'A', etc under X */
+	Kcmd=       0xF100,	/* Rune: beginning of Cmd+'a', Cmd+'A', etc on Mac */
+	Kshiftctl=  0xF200, /* Rune: beginning of Shift+Ctrl+'A', etc under X */
+	Kmod1=      0xF300, /* Rune: beginning of Mod1+'a', Mod1+'A', etc under X */
+	Kshiftmod1= 0xF400, /* Rune: beginning of Shift+Mod1+'A', etc under X */
+
+	/* LW: note that Unicode reserves U+E000–U+F8FF for private use */
 };
 
 #if defined(__cplusplus)
